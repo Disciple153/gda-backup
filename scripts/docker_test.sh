b46b78f11d7c971761452f101bd2872efa9d421f
Canaan Epperson <c_eps@yahoo.com>
2024-06-11 21:57:23 -0700

    working on both actions

--- scripts/docker_test.sh
+++ scripts/docker_test.sh
@@ -5,3 +5,4 @@ docker build -t disciple153/gda-backup:0.0.1 .
 docker compose up -d
 sleep 65
 docker logs gda-backup-gda_backup-1
+docker exec gda-backup-gda_backup-1 gda_backup restore -t "/restore" -b "disciple153-test" -d "gda-backup-test"
