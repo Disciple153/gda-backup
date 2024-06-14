e0f95a7d4e79ac24980d38a4c2d21d963874e3e3
Canaan Epperson <c_eps@yahoo.com>
2024-06-14 14:53:53 -0700

    Enabled multipart upload for large files

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,5 @@
 use assert_cmd::Command;
+use rand::{distributions::Alphanumeric, Rng};
 use std::{fs::{self}, path::Path};
 use serial_test::serial;
 
@@ -312,4 +313,62 @@ fn regex_test_env_dry() {
 
     assert!(!Path::new(&common::build_restore_path(backup_test_file_1)).exists());
     assert!(!Path::new(&common::build_restore_path(backup_test_file_2)).exists());
+}
+
+#[test]
+#[serial]
+fn multipart_upload_test() {
+    // using common code.
+    common::setup();
+
+    fs::create_dir_all(common::TEST_DIR_BACKUP).unwrap();
+    fs::create_dir_all(common::TEST_DIR_RESTORE).unwrap();
+
+    let backup_test_file = "test1.txt";
+
+    let backup_test: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(1024 * 1024 * 500)
+        .map(char::from)
+        .collect();
+
+    common::create_file(backup_test_file, &backup_test);
+
+    let mut backup = Command::cargo_bin("gda_backup").unwrap();
+
+    let backup = backup
+        .arg("backup")
+        .args(&["--target-dir", common::TEST_DIR_BACKUP])
+        .args(&["--bucket-name", "disciple153-test"])
+        .args(&["--dynamo-table", "gda-backup-test"])
+        .args(&["--db-engine", common::DB_ENGINE])
+        .args(&["--postgres-user", common::POSTGRES_USER])
+        .args(&["--postgres-password", common::POSTGRES_PASSWORD])
+        .args(&["--postgres-host", common::POSTGRES_HOST])
+        .args(&["--postgres-db", common::POSTGRES_DB])
+        .args(&["--min-storage-duration", "1"]);
+
+    let mut restore = Command::cargo_bin("gda_backup").unwrap();
+
+    let restore = restore
+        .arg("restore")
+        .args(&["--target-dir", common::TEST_DIR_RESTORE])
+        .args(&["--bucket-name", "disciple153-test"])
+        .args(&["--dynamo-table", "gda-backup-test"]);
+
+    let assert_backup = backup.assert();
+    dbg!(assert_backup.get_output());
+
+    assert_backup.success();
+    
+    let assert_restore = restore.assert();
+    dbg!(assert_restore.get_output());
+
+    assert_restore.success();
+
+    dbg!(common::build_restore_path(backup_test_file));
+
+    let restore_test1 = common::read_file(backup_test_file).unwrap();
+
+    assert_eq!(backup_test, restore_test1);
 }
